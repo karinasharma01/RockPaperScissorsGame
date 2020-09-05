2e7076d768d74d64229f48f12594a35a691af085
karinasharma01 <68894444+karinasharma01@users.noreply.github.com>
2020-09-05 15:08:15 +0530

    Animations and Score Update Feature Added!

--- game.js
+++ game.js
@@ -1,18 +1,116 @@
 const game = () => {
-    let playerScore=0;
-    let computerScore=0;
+    let pScore = 0;
+    let cScore = 0;
+  
+    //Start the Game
     const startGame = () => {
-        const playButton = document.querySelector('.start button');
-        const introScreen=document.querySelector('.start');
-        const start=document.querySelector('.game');
-
-        playButton.addEventListener('click', () => {
-            introScreen.classList.add('fadeOut');
-        });
-
+      const playButton = document.querySelector(".start button");
+      const introScreen = document.querySelector(".start");
+      const match = document.querySelector(".match");
+  
+      playButton.addEventListener("click", () => {
+        introScreen.classList.add("fadeOut");
+        match.classList.add("fadeIn");
+      });
     };
-    //calling the inner functions
+    //Play Match
+    const playMatch = () => {
+      const options = document.querySelectorAll(".options button");
+      const playerHand = document.querySelector(".player-hand");
+      const computerHand = document.querySelector(".computer-hand");
+      const hands = document.querySelectorAll(".hands img");
+  
+      hands.forEach(hand => {
+        hand.addEventListener("animationend", function() {
+          this.style.animation = "";
+        });
+      });
+      //Computer Options
+      const computerOptions = ["rock", "paper", "scissors"];
+  
+      options.forEach(option => {
+        option.addEventListener("click", function() {
+          //Computer Choice
+          const computerNumber = Math.floor(Math.random() * 3);
+          const computerChoice = computerOptions[computerNumber];
+  
+          setTimeout(() => {
+            //Here is where we call compare hands
+            compareHands(this.textContent, computerChoice);
+            //Update Images
+            playerHand.src = `./assets/${this.textContent}.png`;
+            computerHand.src = `./assets/${computerChoice}.png`;
+          }, 2000);
+          //Animation
+          playerHand.style.animation = "shakePlayer 2s ease";
+          computerHand.style.animation = "shakeComputer 2s ease";
+        });
+      });
+    };
+  
+    const updateScore = () => {
+      const playerScore = document.querySelector(".player-score p");
+      const computerScore = document.querySelector(".computer-score p");
+      playerScore.textContent = pScore;
+      computerScore.textContent = cScore;
+    };
+  
+    const compareHands = (playerChoice, computerChoice) => {
+      //Update Text
+      const winner = document.querySelector(".winner");
+      //Checking for a tie
+      if (playerChoice === computerChoice) {
+        winner.textContent = "It is a draw!Phew!";
+        return;
+      }
+      //Check if Player selects rock
+      if (playerChoice === "rock") {
+        if (computerChoice === "scissors") {
+          winner.textContent = "Player Wins!Congrats!";
+          pScore++;
+          updateScore();
+          return;
+        } else {
+          winner.textContent = "Computer Wins!Better Luck Next Time:(!";
+          cScore++;
+          updateScore();
+          return;
+        }
+      }
+      //Check for Paper
+      if (playerChoice === "paper") {
+        if (computerChoice === "scissors") {
+          winner.textContent = "Computer Wins!Better Luck Next Time:(!";
+          cScore++;
+          updateScore();
+          return;
+        } else {
+          winner.textContent = "Player Wins";
+          pScore++;
+          updateScore();
+          return;
+        }
+      }
+      //Check for Scissors
+      if (playerChoice === "scissors") {
+        if (computerChoice === "paper") {
+          winner.textContent = "Player Wins!Congrats!";
+          pScore++;
+          updateScore();
+          return;
+        } else {
+          winner.textContent = "Computer Wins!Better Luck Next Time:(!";
+          cScore++;
+          updateScore();
+          return;
+        }
+      }
+    };
+  
+    //Is call all the inner function
     startGame();
-};
-//calling the main function game to start a match
-game();
+    playMatch();
+  };
+  
+  //start the game function
+  game();
